9abdddb0bdad2164443c9e7bc2cf1cb67dd30aba
Baraa <baraatv2006@gmail.com>
2026-08-22 01:38:33 +0300

    8

--- the_test.js
+++ the_test.js
@@ -2,11 +2,14 @@ let counter = document.getElementById("count");
 let sec = 3;
 counter.textContent = sec
 
-setInterval(() => {
+const timer = setInterval(() => {
     if(sec > 1){
     sec = sec - 1
     counter.textContent = sec
     
+}else if(sec == 1){
+    counter.style.display="none"
+ clearInterval(timer);
 }
 
 }, 1000);
