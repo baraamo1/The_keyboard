cd424af9376598809841d0553178686848310fc3
Baraa <baraatv2006@gmail.com>
2026-08-23 15:19:25 +0300

    10

--- the_test.js
+++ the_test.js
@@ -1,15 +1,30 @@
 let counter = document.getElementById("count");
+let keyboard = document.getElementById("keyboard");
 let sec = 3;
-counter.textContent = sec
+let tiemer = document.getElementById("timer");
+let down = 60;
+counter.textContent = sec;
+keyboard.style.display = "none";
+tiemer.textContent = down;
 
 const timer = setInterval(() => {
-    if(sec > 1){
-    sec = sec - 1
-    counter.textContent = sec
-    
-}else if(sec == 1){
-    counter.style.display="none"
- clearInterval(timer);
+  if (sec > 1) {
+    sec = sec - 1;
+    counter.textContent = sec;
+  } else if (sec == 1) {
+    counter.style.display = "none";
+    clearInterval(timer);
+    keyboard.style.display = "block ";
+    sec --
+  }
+}, 1000);
+const downtime = setInterval(() => {
+    if(sec == 0){
+  if (down > 1) {
+    down = down - 1;
+    tiemer.textContent = down;
+  } else if (down == 1) {
+    clearInterval(downtime);
+  }
 }
-
 }, 1000);
