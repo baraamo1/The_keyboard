aeb2d81b3966f67654e541230d1fcc8bcfd4025f
baraa <baraatv2006@gmail.com>
2026-08-30 04:06:01 +0300

    game card

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
+const select1 = document.getElementById("select");
+const select2 = document.getElementById("select2");
 
-
+let num = 0;
 document.addEventListener("keydown", (event) => {
-if(event.key === "Enter")
-    window.location.href = "test.html";
+  if (event.key == "ArrowRight" || event.key == "ArrowLeft") {
+    num++;
+    if (num % 2 == 0) {
+      select1.style.border = "10px solid black";
+      select2.style.border = "2px solid black";
+
+    } else {
+      select2.style.border = "10px solid black";
+      select1.style.border = "2px solid black";
+    }
+  }
+
+if(event.key === "Enter" && num % 2 == 0){
+ window.location.href = "test.html";
+}else if(event.key === "Enter" && num % 2 != 0){
+    window.location.href = "game.html";
+}
+
 });
 
-let imgno = document.getElementById("no")
+let imgno = document.getElementById("no");
 
 document.addEventListener("click", (event) => {
-imgno.style.left = `${event.clientX}px`;
-imgno.style.top = `${event.clientY}px`;
-    imgno.style.display = "block"
-
+  imgno.style.left = `${event.clientX}px`;
+  imgno.style.top = `${event.clientY}px`;
+  imgno.style.display = "block";
 });
+
+
+
+
+
+/*
+
+if(event.key === "Enter" && num % 2 == 0){
+ window.location.href = "test.html";
+}else if(event.key === "Enter" && num % 2 != 0){
+    window.location.href = "game.html";
+}
+   */
