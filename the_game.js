7d60dcddfe9bcac23406769222951bd97bee4365
baraa <baraatv2006@gmail.com>
2026-08-31 05:38:55 +0300

    shoot1

--- the_game.js
+++ the_game.js
@@ -11,48 +11,31 @@ let x2 = 25
 
 document.addEventListener("keydown", (event) => {
   if (event.key === "ArrowRight") {
-    if (the_letter < 27) {
+    if (the_letter < 25) {
       the_letter++;
 
       keys[the_letter].style.border = "solid";
       keys[the_letter - 1].style.border = "none";
 
-      if (the_letter == 28) {
-        submit.style.border = "solid";
-        keys[the_letter - 1].style.border = "none";
-      }
+      
     }
   }
 
-  if (
-    event.key === "Enter" &&
-    the_letter != -1 &&
-    the_letter != 26 &&
-    the_letter != 27 
-  ) {
-    savedletters[letternum] = keys[the_letter].textContent;
-    letternum++;
+  if (event.key === "Enter") {
 
-    wroten.textContent = savedletters.join("");
-  }
-  //next
-  if (event.key === "Enter" && the_letter == 26) {
-    if (wroten.textContent == randwords[random]) {
-      correct ++;
-      console.log(`corrict = ${correct}`);
 
-    } else if (the_word.textContent == "") {
-      console.log("write");
-    } else {
-      wrong ++;
-console.log(`WRONGE = ${wrong}`) 
-    }
-    numberOfQs ++
-    random = Math.floor(Math.random() * 25);
-    the_word.textContent = randwords[random];
-    savedletters = []
-    wroten.textContent = ""
+
+
+
+
+
+
+
+    
+       shoot1[the_letter].style.display ="block"
+
   }
+  
 
 
   if (event.key === "ArrowLeft") {
@@ -89,24 +72,7 @@ document.addEventListener("click", (event) => {
 
 
 
-function shoot (){
-    document.addEventListener("keydown", (event) => {
 
-      if(event.key == ""){}
-
-
-
-
-
-
-
-
-
-
-
-
-    })
-}
 
 
 
